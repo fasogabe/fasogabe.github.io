f357c2db1d6d4e12fa477400a38a79e385c33215
fasogabe <omicronesta@gmail.com>
2019-04-04 14:45:26 -0700

    working on lightbox/added words to home

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # bighouse
+
+### todo
+*homepage*
+- add more words and pictures and info
+
+*gallery*
+- add lightbox function
+- add more photos
+
+*rates*
+- add rates table
+
+*contact*
+- add contact info
+
+*general*
+- add toggle switch for cycling through color schemes
+- make sure evrything is responsive / test on mobile
+- add fonts
---
+++ js/gallery.js
@@ -0,0 +1,32 @@
+// open modal
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+}
+
+// close modal
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// next/prev
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
